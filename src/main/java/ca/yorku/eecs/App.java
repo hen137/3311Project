8d75faeb59c20493f8d52ed846931393aea4b613
Henry Abramovich <henry.abramovich@gmail.com>
2024-07-22 00:37:08 -0400

    refactored context handlers as constants

--- src/main/java/ca/yorku/eecs/App.java
+++ src/main/java/ca/yorku/eecs/App.java
@@ -6,33 +6,34 @@ import com.sun.net.httpserver.HttpServer;
 
 public class App 
 {
+    public static final Actor ACTOR = new Actor();
+    public static final Movie MOVIE = new Movie();
+    public static final Relationship RELATIONSHIP = new Relationship();
+    public static final Bacon BACON = new Bacon();
+    public static final DoS DOS = new DoS();
+
     static int PORT = 8080;
+
     public static void main(String[] args) throws IOException
     {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
 
-        Actor actor = new Actor();
-        Movie movie = new Movie();
-        Relationship relationship = new Relationship();
-        Bacon bacon = new Bacon();
-        DoS dos = new DoS();
+        server.createContext("/api/v1/addActor/", ACTOR);
+        server.createContext("/api/v1/getActor/", ACTOR);
+        server.createContext("/api/v1/allMovies", ACTOR);
 
-        server.createContext("/api/v1/addActor/", actor);
-        server.createContext("/api/v1/getActor/", actor);
-        server.createContext("/api/v1/allMovies", actor);
+        server.createContext("/api/addMovie/", MOVIE);
+        server.createContext("/api/getMovie/", MOVIE);
+        server.createContext("/api/allActors/", MOVIE);
 
-        server.createContext("/api/addMovie/", movie);
-        server.createContext("/api/getMovie/", movie);
-        server.createContext("/api/allActors/", movie);
+        server.createContext("/api/addRelationship/", RELATIONSHIP);
+        server.createContext("/api/hasRelationship/", RELATIONSHIP);
 
-        server.createContext("/api/addRelationship/", relationship);
-        server.createContext("/api/hasRelationship/", relationship);
+        server.createContext("/api/computeBaconNumber/", BACON);
+        server.createContext("/api/computeBaconPath/", BACON);
 
-        server.createContext("/api/computeBaconNumber/", bacon);
-        server.createContext("/api/computeBaconPath/", bacon);
-
-        server.createContext("/api/computeDoSNumber/", dos);
-        server.createContext("/api/computeDoSPath/", dos);
+        server.createContext("/api/computeDoSNumber/", DOS);
+        server.createContext("/api/computeDoSPath/", DOS);
 
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
