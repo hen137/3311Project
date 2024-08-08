553993e18b2503e3b67551ea29d7633910be450a
HyunjiYun <96277611+Hyunji-Yun@users.noreply.github.com>
2024-08-08 12:11:17 -0400

    Update Bacon.java

--- src/main/java/ca/yorku/eecs/Bacon.java
+++ src/main/java/ca/yorku/eecs/Bacon.java
@@ -33,7 +33,29 @@ public class Bacon implements HttpHandler {
     }
 
     public void handleGetNumber(HttpExchange r) {
- 
+         String queryStr = String.format("MATCH (a:Actor {id: '%s'}), (b:Actor {id: 'nm0000102'}), " +
+                    "p=shortestPath((a)-[:ACTED_IN*]-(b)) " +
+                    "RETURN length(p) AS baconNumber", actorId);
+            StatementResult result = session.run(queryStr);
+
+            if (result.hasNext()) {
+                int baconNumber = result.next().get("baconNumber").asInt();
+
+                JSONObject response = new JSONObject();
+                response.put("actorId", actorId);
+                response.put("baconNumber", baconNumber);
+
+                String responseText = response.toString();
+                r.sendResponseHeaders(200, responseText.length()); 
+                OutputStream os = r.getResponseBody();
+                os.write(responseText.getBytes());
+                os.close();
+            } else {
+                r.sendResponseHeaders(404, -1); 
+            }
+        } catch (Exception e) {
+            r.sendResponseHeaders(500, -1); 
+        }
 
     }
 
