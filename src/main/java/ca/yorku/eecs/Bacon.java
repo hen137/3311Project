72913d002ee3f3f41ec6353cab6ef42a1d8cbd8f
HyunjiYun <96277611+Hyunji-Yun@users.noreply.github.com>
2024-08-08 12:07:52 -0400

    Update Bacon.java

--- src/main/java/ca/yorku/eecs/Bacon.java
+++ src/main/java/ca/yorku/eecs/Bacon.java
@@ -12,11 +12,28 @@ public class Bacon implements HttpHandler {
 
     }
 
-    public void handle(HttpExchange r) {
+    public void handle(HttpExchange r) throws IOException {
+        try {
+            if (r.getRequestMethod().equals("GET")) {
+                String path = r.getRequestURI().getPath();
+                if (path.endsWith("/computeBaconNumber/")) {
+                    handleGetNumber(r);
+                } else if (path.endsWith("/computeBaconPath/")) {
+                    handleGetPath(r);
+                } else {
+                    r.sendResponseHeaders(404, -1);
+                }
+            } else {
+                r.sendResponseHeaders(405, -1);
+            }
+        } catch (Exception e) {
+            r.sendResponseHeaders(500, -1);
+        }
 
     }
 
     public void handleGetNumber(HttpExchange r) {
+ 
 
     }
 
