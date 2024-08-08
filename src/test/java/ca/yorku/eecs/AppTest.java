20fc252fa6107564916be22757043a7a1e05e23a
Charlene Noye <cnoye18@gmail.com>
2024-08-08 19:31:52 -0400

    Endpoint Testcases added

--- src/test/java/ca/yorku/eecs/AppTest.java
+++ src/test/java/ca/yorku/eecs/AppTest.java
@@ -3,6 +3,10 @@ package ca.yorku.eecs;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import io.restassured.response.Response;
+
 
 /**
  * Unit test for simple App.
@@ -35,4 +39,117 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @org.junit.Test
+    public void addActorPass() {
+        String payload = "{ \"name\": \"Leonardo DiCaprio\", \"actorID\": \"ld10011\" }";
+        Response response = given()
+                .contentType("application/json")
+                .body(payload)
+                .when()
+                .put("/api/v1/addActor");
+        assertEquals(200, response.getStatusCode());
+        assertEquals("Pass added successfully", response.jsonPath().getString("message"));
+    }
+
+    @org.junit.Test
+    public void addActorFail() {
+        String payload = "{ \"name\": \"Denzel Washington\"}";
+        Response response = given()
+                .contentType("application/json")
+                .body(payload)
+                .when()
+                .put("/api/v1/addActor");
+        assertEquals(400, response.getStatusCode());
+        assertEquals("Fail to add actor", response.jsonPath().getString("message"));
+    }
+
+    @org.junit.Test
+    public void addMovieFail() {
+        String payload = "{ \"movieID\": \"ps24311\" }";
+        Response response = given()
+                .contentType("application/json")
+                .body(payload)
+                .when()
+                .put("/api/v1/addMovie");
+        assertEquals(400, response.getStatusCode());
+        assertEquals("Fail to add movie", response.jsonPath().getString("message"));
+    }
+
+    @org.junit.Test
+    public void addMoviePass() {
+        String payload = "{ \"name\": \"Parasite\", \"movieID\": \"ps24311\" }";
+        Response response = given()
+                .contentType("application/json")
+                .body(payload)
+                .when()
+                .put("/api/v1/addMovie");
+        assertEquals(200, response.getStatusCode());
+        assertEquals("Pass added successfully", response.jsonPath().getString("message"));
+    }
+
+    @org.junit.Test
+    public void addRelationshipPass() {
+        String payload = "{ \"actorID\": \"ld10011\", \"movieID\": \"ps24311\" }";
+        Response response = given()
+                .contentType("application/json")
+                .body(payload)
+                .when()
+                .put("/api/v1/addRelationship");
+        assertEquals(200, response.getStatusCode());
+        assertEquals("Pass added successfully", response.jsonPath().getString("message"));
+    }
+
+    @org.junit.Test
+    public void addRelationshipFail() {
+        String payload = "{ \"actorID\": \"ld10011\" }";
+        Response response = given()
+                .contentType("application/json")
+                .body(payload)
+                .when()
+                .put("/api/v1/addRelationship");
+        assertEquals(400, response.getStatusCode());
+        assertEquals("Fail to add relationship", response.jsonPath().getString("message"));
+    }
+
+    @org.junit.Test
+    public void getActorPass() {
+        Response response = given()
+                .contentType("application/json")
+                .when()
+                .get("/api/v1/getActor/ld10011");
+        assertEquals(200, response.getStatusCode());
+        assertEquals("Leonardo DiCaprio", response.jsonPath().getString("name"));
+    }
+
+    @org.junit.Test
+    public void getActorFail() {
+        Response response = given()
+                .contentType("application/json")
+                .when()
+                .get("/api/v1/getActor/ld10012");
+        assertEquals(400, response.getStatusCode());
+        assertEquals("Fail to get actor", response.jsonPath().getString("message"));
+    }
+
+    @org.junit.Test
+    public void getMoviePass() {
+        Response response = given()
+                .contentType("application/json")
+                .when()
+                .get("/api/v1/getMovie/ps24311");
+        assertEquals(200, response.getStatusCode());
+        assertEquals("Parasite", response.jsonPath().getString("name"));
+    }
+
+    @org.junit.Test
+    public void getMovieFail() {
+        Response response = given()
+                .contentType("application/json")
+                .when()
+                .get("/api/v1/getMovie/ps24312");
+        assertEquals(400, response.getStatusCode());
+        assertEquals("Fail to get movie", response.jsonPath().getString("message"));
+    }
+    
 }
