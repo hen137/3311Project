e2eda1962d99bd7bd0833782b9f2b5712b984094
HyunjiYun <96277611+Hyunji-Yun@users.noreply.github.com>
2024-08-11 15:03:30 -0400

    Update App.java

--- src/main/java/ca/yorku/eecs/App.java
+++ src/main/java/ca/yorku/eecs/App.java
@@ -17,22 +17,22 @@ public class App {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
 
-        server.createContext("/api/v1/addActor/", ACTOR);
-        server.createContext("/api/v1/getActor/", ACTOR);
-        server.createContext("/api/v1/checkRelatedActors/", ACTOR);
+        server.createContext("/api/v1/addActor", ACTOR);
+        server.createContext("/api/v1/getActor", ACTOR);
+        server.createContext("/api/v1/checkRelatedActors", ACTOR);
 
-        server.createContext("/api/v1/addMovie/", MOVIE);
-        server.createContext("/api/v1/getMovie/", MOVIE);
-        server.createContext("/api/v1/getRelatedMovies/", MOVIE);
+        server.createContext("/api/v1/addMovie", MOVIE);
+        server.createContext("/api/v1/getMovie", MOVIE);
+        server.createContext("/api/v1/getRelatedMovies", MOVIE);
 
-        server.createContext("/api/v1/addRelationship/", RELATIONSHIP);
-        server.createContext("/api/v1/hasRelationship/", RELATIONSHIP);
+        server.createContext("/api/v1/addRelationship", RELATIONSHIP);
+        server.createContext("/api/v1/hasRelationship", RELATIONSHIP);
 
-        server.createContext("/api/v1/computeBaconNumber/", BACON);
-        server.createContext("/api/v1/computeBaconPath/", BACON);
+        server.createContext("/api/v1/computeBaconNumber", BACON);
+        server.createContext("/api/v1/computeBaconPath", BACON);
 
-        server.createContext("/api/v1/computeDoSNumber/", DOS);
-        server.createContext("/api/v1/computeDoSPath/", DOS);
+        server.createContext("/api/v1/computeDoSNumber", DOS);
+        server.createContext("/api/v1/computeDoSPath", DOS);
 
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
